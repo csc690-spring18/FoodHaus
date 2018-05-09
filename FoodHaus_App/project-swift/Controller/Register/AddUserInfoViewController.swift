f4a0e41111519bb5e20373c236c46cb908957145
boli <bojms45@gmail.com>
2018-05-08 23:50:22 -0700

    Hide Keyboard when click anywhere beside textfield

--- FoodHaus_App/project-swift/Controller/Register/AddUserInfoViewController.swift
+++ FoodHaus_App/project-swift/Controller/Register/AddUserInfoViewController.swift
@@ -15,7 +15,8 @@ class AddUserInfoViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.hideKeyboardWhenTappedAround()
+
         Auth.auth().addStateDidChangeListener { auth, user in
             guard let user = user else { return }
             self.user = Users(authData: user)
